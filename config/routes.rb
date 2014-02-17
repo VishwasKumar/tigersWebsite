461eba06cb769124b9990b229aad952db61debcd
Vishwas V Kumar <vishwaskumar058@gmail.com>
2014-02-17 12:32:40 +0530

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ SFWebsite::Application.routes.draw do
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-root 'sf/index'
+root 'sf#index'
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"

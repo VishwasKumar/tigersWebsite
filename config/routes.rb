115c55a65aa5879336f33e6307258944bb5d6f52
Vishwas V Kumar <vishwaskumar058@gmail.com>
2014-02-17 12:26:39 +0530

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ end
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/sf/index"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"

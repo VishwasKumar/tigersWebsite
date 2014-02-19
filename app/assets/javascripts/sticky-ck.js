2a5576b1344e400d6296b83cd879862af67e39fb
Vishwas V Kumar <vishwaskumar058@gmail.com>
2014-02-19 17:55:10 +0530

    minor bug fixes

--- app/assets/javascripts/sticky-ck.js
+++
@@ -1,1 +0,0 @@
-$(function(){if(!!$(".donatenow").offset()){var e=$(".donatenow").offset().top;$(window).scroll(function(){var t=$(window).scrollTop();e<t?$(".donatenow").css({position:"fixed",top:0}):$(".donatenow").css("position","static")})}});
